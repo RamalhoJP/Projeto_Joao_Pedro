390c76a50a0b32d40bbad07af9c600da1cc2580a
João Pedro Ramalho <joaopedroramalho249@gmail.com>
2023-09-25 18:32:17 -0300

    Inserindo a classe D

---
+++ D.java
@@ -0,0 +1,23 @@
+public class D {
+    private String D1;
+    private int D2;
+
+    public String getD1() {
+        return D1;
+    }
+    public void setD1(String d1) {
+        D1 = d1;
+    }
+    public int getD2() {
+        return D2;
+    }
+    public void setD2(int d2) {
+        D2 = d2;
+    }
+    public void MD1(){
+        System.out.println("MD1");
+    }
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
